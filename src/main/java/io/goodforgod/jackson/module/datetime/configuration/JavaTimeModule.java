7597183853493312362a5836ec35f8b811fa64b7
Anton Kurako <goodforgod.dev@gmail.com>
2022-01-19 00:21:10 +0300

    [1.0.0-SNAPSHOT] JavaTimeModule cleanup

--- src/main/java/io/goodforgod/jackson/module/datetime/configuration/JavaTimeModule.java
+++ src/main/java/io/goodforgod/jackson/module/datetime/configuration/JavaTimeModule.java
@@ -2,13 +2,8 @@ package io.goodforgod.jackson.module.datetime.configuration;
 
 import com.fasterxml.jackson.databind.BeanDescription;
 import com.fasterxml.jackson.databind.DeserializationConfig;
-import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.deser.ValueInstantiator;
 import com.fasterxml.jackson.databind.deser.ValueInstantiators;
-import com.fasterxml.jackson.databind.deser.std.StdValueInstantiator;
-import com.fasterxml.jackson.databind.introspect.AnnotatedClass;
-import com.fasterxml.jackson.databind.introspect.AnnotatedClassResolver;
-import com.fasterxml.jackson.databind.introspect.AnnotatedMethod;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.jsr310.PackageVersion;
 import com.fasterxml.jackson.datatype.jsr310.deser.*;
@@ -319,56 +314,8 @@ public class JavaTimeModule extends SimpleModule {
             public ValueInstantiator findValueInstantiator(DeserializationConfig config,
                                                            BeanDescription beanDesc,
                                                            ValueInstantiator defaultInstantiator) {
-                JavaType type = beanDesc.getType();
-                Class<?> raw = type.getRawClass();
-
-                // 15-May-2015, tatu: In theory not safe, but in practice we do need to do "fuzzy" matching
-                // because we will (for now) be getting a subtype, but in future may want to downgrade
-                // to the common base type. Even more, serializer may purposefully force use of base type.
-                // So... in practice it really should always work, in the end. :)
-                if (ZoneId.class.isAssignableFrom(raw)) {
-                    // let's assume we should be getting "empty" StdValueInstantiator here:
-                    if (defaultInstantiator instanceof StdValueInstantiator) {
-                        StdValueInstantiator inst = (StdValueInstantiator) defaultInstantiator;
-                        // one further complication: we need ZoneId info, not sub-class
-                        AnnotatedClass ac;
-                        if (raw == ZoneId.class) {
-                            ac = beanDesc.getClassInfo();
-                        } else {
-                            // we don't need Annotations, so constructing directly is fine here
-                            // even if it's not generally recommended
-                            ac = AnnotatedClassResolver.resolve(config,
-                                    config.constructType(ZoneId.class), config);
-                        }
-                        if (!inst.canCreateFromString()) {
-                            AnnotatedMethod factory = _findFactory(ac, "of", String.class);
-                            if (factory != null) {
-                                inst.configureFromStringCreator(factory);
-                            }
-                            // otherwise... should we indicate an error?
-                        }
-                        // return ZoneIdInstantiator.construct(config, beanDesc, defaultInstantiator);
-                    }
-                }
                 return defaultInstantiator;
             }
         });
-    }
-
-    protected AnnotatedMethod _findFactory(AnnotatedClass cls, String name, Class<?>... argTypes) {
-        final int argCount = argTypes.length;
-        for (AnnotatedMethod method : cls.getFactoryMethods()) {
-            if (!name.equals(method.getName()) || (method.getParameterCount() != argCount)) {
-                continue;
-            }
-
-            for (int i = 0; i < argCount; ++i) {
-                Class<?> argType = method.getParameter(i).getRawType();
-                argType.isAssignableFrom(argTypes[i]);
-            }
-
-            return method;
-        }
-        return null;
     }
 }
